97948c056c89deefbdb81c8d869522b9d3ada5e2
clintonventura <clintonkenedyventura@gmail.com>
2022-10-17 02:31:05 -0500

    login con github

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+class SocialiteController extends Controller
+{
+    //
+    public function redirectGithub(){
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function callbackGithub(){
+        try {
+
+            $githubUser = Socialite::driver('github')->user();
+            $findUser = User::where('gh_id',$githubUser->id)->first();
+            if($findUser){
+                Auth::login($findUser);
+                return redirect()->intended('home');
+
+            }
+            else{
+                $newUser = User::create([
+                    'name'=>$githubUser->nickname,
+                    'email'=>$githubUser->email,
+                    'gh_id'=>$githubUser->id,
+                    'password'=>encrypt('12345678')
+                ]);
+                Auth::login($newUser);
+                return redirect()->intended('home');
+            }
+        }catch (Exception $e){
+            dd($e->getMessage());
+        }
+    }
+}
